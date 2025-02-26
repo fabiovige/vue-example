d8f917caad9c58d557258e921bc25843e12f3f48
Fabio Vige <fabiovige@gmail.com>
2025-02-26 07:56:38 -0300

    instalando axios

---
+++ src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const login = () => {
+    console.log('Login');
+}
+</script>
+
+<template>
+    <div>
+        <h1>Login</h1>
+        <form @submit.prevent="login">
+            <input type="text" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</template>
+
